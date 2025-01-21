7677192f9cb9fe0f81f2e33589f6f7507dd7c93d
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-01-21 10:08:45 -0700

    Add files via upload

---
+++ PHPcomments.php
@@ -0,0 +1,10 @@
+<?php
+print("There are Comments on this page but you cannot see them!") //This is an example of a comment
+
+/* this is a multi line comment
+     no matter how many lines of text I put
+    As long as it is between the 
+    / * * / it will be counted as text
+*/
+?>
+
