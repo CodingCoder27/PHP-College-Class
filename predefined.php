42bcc083389e002150c3614d8271a48495183888
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-01-26 14:26:24 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang = "en">
+    <head>
+        <meta charset = "utf-8">
+        <title> Predefiend Variables</title>
+    </head>
+    <body>
+        <pre>
+            <?php // Script 2.1 - predefiend.php
+
+            //print_r($_SERVER) will show the value of $_SERVER
+                print_r($_SERVER)
+            ?>
+        </pre>
+    </body>
+</html>
+
+
