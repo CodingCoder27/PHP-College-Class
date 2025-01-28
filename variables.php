097c82173199cd5ba93979a802309c0f5c5e3318
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-01-27 18:53:44 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title> Variables</title>
+</head>
+<body>
+    <?php //Script 2.3 -- variables.php
+        $first_name="Benjamin";
+        $last_name="Tallman";
+        $address="7891 W. Sunset St.";
+        $phone_number=1325469065;
+
+        print "<p>The address is: <br>$address <br>, my name is <br> $first_name <br> <br>$last_name <br> and my phone number is <br> $phone_number <br>";
+    ?>
+</body>
