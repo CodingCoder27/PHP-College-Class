e1c603ffacd52a5107cc1fe083e305d24297378a
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-02-03 12:55:17 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <title>Random Number</title>
+        </head>
+        <body>
+            <h1>Random Number</h1>
+            <p>Here is a random number between 10 and 100: <?php echo(rand(10, 100) . "<br>");?> </p>
+        </body>
+    </html>
