6c382971f230aed0b440e3151fa4d61b50a3ab57
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-02-10 11:34:37 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration</title>
+    </head>
+    <body>
+        <h1>Registration Results</h1>
+        <?php //script 6.2
+
+        $okay = true;
+
+        if ($okay) {
+            print '<p> You have been successfully registered (but not really).</p> <br>';
+        }
+        ?>
+    </body>
+</html>
