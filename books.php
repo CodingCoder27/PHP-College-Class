e575b54cbb8f658911f4a24f1a791adb65e46179
CodingCoder27 <159964699+CodingCoder27@users.noreply.github.com>
2025-02-17 11:16:07 -0700

    Add files via upload

--- books.php
+++ books.php
@@ -1,36 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="utf-8">
-    <title>Larry Ullman's Books and Chapters</title>
-    </head>
+<?php // Script 8.5 - books.php
 
-    <body>
-    <h1>Some of Larry Ullman's Books</h1>
-        <?php // Script 7.4 - books.php
+// Set the page title and include the header file:
+define('TITLE', 'Books by J.D. Salinger');
+include('header.html');
+?>
 
-        // Create the first array:
-        $phpvqs = [1 => 'Getting Started with PHP', 'Variables', 'HTML Forms and PHP', 'Using Numbers'];
+<h2>J.D. Salinger's Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
 
-        // Create the second array:
-        $phpadv = [1 => 'Advanced PHP Techniques', 'Developing Web Applications', 'Advanced Database Concepts', 'Basiv Object-Oriented Programming'];
-
-        // Create the third array:
-        $phpmysql = [1 => 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'];
-
-        // Create the multidimensional array:
-        $books = array ('PHP VQS' => $phpvqs, 'PHP Advanced VQP' => $phpadv, 'PHP and MySQL VQP' => $phpmysql);
-        // Print out some values:
-        print "<p>The third chapter of my first book is <i>{$books['PHP VQS'][3]}</i>.</p>";
-        print"<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-        print "<p>The fourth chapter of my fourth book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p>";
-
-        // See what happens with foreach:
-        foreach ($books as $key => $value) {
-        print_r($value);
-        print("\n"); //"<p>$key: $value</p>\n"; is not working, keep getting error that array is not being converted to string
-        }
-
-        ?>
-    </body>
-</html>
+<?php include('footer.html'); ?>
